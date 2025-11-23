2939d91c54282eff15658011fc6b9185e62f5e25
ignasgin <ignasgin@gmail.com>
2025-11-23 16:17:50 +0200

    Configure Whitenoise to serve static files with proper MIME types

--- Trainee/settings.py
+++ Trainee/settings.py
@@ -183,12 +183,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Use whitenoise for static files with proper MIME types
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Trainee/urls.py
+++ Trainee/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import FileResponse, Http404
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenRefreshView
 from core.jwt_serializers import CustomTokenObtainPairView
-from core.frontend_views import index, serve_static
+from core.frontend_views import index
 import os
 
 urlpatterns = [
@@ -22,12 +21,10 @@ urlpatterns = [
     # OpenAPI UI
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    
-    # Serve frontend static assets with proper MIME types
-    re_path(r'^assets/(?P<path>.*)$', serve_static, name='frontend-assets'),
 ]
 
 # React frontend - catch all routes (must be last)
+# Whitenoise will handle /assets/ automatically via STATIC_URL
 urlpatterns += [
     re_path(r'^', index, name='index'),
 ]
